e062bc9016210e9234463b1a92ea8a483ebf112f
Ev1lGoldFish <63042619+Ev1lGoldFish@users.noreply.github.com>
2024-10-24 13:08:55 -0400

    Update index.jsx

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -19,37 +19,30 @@ const Work = () => {
       title: 'Bright Mind Enrichment and Schooling',
       role: 'Full Stack Developer',
       description: [
-        "Spearheaded front-end development for the organization's web application utilizing React and Tailwind and achieved a significant increase in user interaction metrics with a boost in daily active users reaching over 2,000.",
+        "•	Spearheaded front-end development for the organization's web application utilizing React and Tailwind and achieved a significant increase in user interaction metrics with a boost in daily active users reaching over 2,000.",
         'Implemented REST APIs in Java with Spring Boot to provide content to front-end, ensuring seamless data handling and improved server response times for thousands of monthly users.',
         "Developed a secure authentication system utilizing JWT that protected user data for over 10,000 monthly users and ensured compliance with GDPR and CCPA regulations while reinforcing trust in the platform's security measures.",
         'Coordinated the deployment and scaling of the application using Docker and AWS EC2, achieving consistent performance and reliability across multiple production environments.',
+        'Participated in full SDLC including requirements gathering, database schema design, and code optimization for enhanced application performance and stability.'
       ],
       expanded: false,
     },
     {
-      date: 'October, 2020-May, 2022',
+      date: 'September 2018 - May, 2022',
       title: 'Infosys Ltd.',
       role: 'Associate Consultant',
       description: [
-        'Led Accounting for Clinical Trails (ACT) project for Novartis Pharma AG, analyzing financial data from clinical trial interfaces and achieved a remarkable 50% reduction in Idoc processing times by optimizing ETL pipelines.',
-        'Designed and developed interactive reports and dynamic dashboards using Power BI, Tableau and Looker, enabling global users to extract key insights and make informed business decisions.',
-        'Managed enhancements by examining business requirements to improve existing functionalities and pipelines using SQL, resulting in a remarkable 60% reduction in data extraction, transformation, aggregation times.',
-        'Collaborated with cross-functional teams and end users to design and maintain visual reporting tools that captured critical metrics and streamlined data visualization processes for clinical and operational performance.',
-      ],
-      expanded: false,
-    },
-    {
-      date: 'September 2018 - October 2020',
-      title: 'Infosys Ltd.',
-      role: 'Systems Engineer',
-      description: [
-        'Developed production ready REST APIs for LKQ corporation’s sales workbench application using Java Spring boot framework.',
-        'Utilized Spring JPA, Hibernate procedural calls for data retrieval from database and Apache Kafka to reduce API response time by 40%.',
-        'Deployed Spring Boot-based microservices in Docker and Amazon EC2 container using Jenkins. Executed unit test cases by using JUNIT for accuracy of code and logging with log4j.',
+        'Developed production ready RESTful APIs for Registration, creating and updating Order, Cart Summary, and Payment services using Java Spring boot framework.',
+        'Implemented Persistence layer using Spring JPA, Hibernate to interact with the database, Used Hibernate Framework for object-relational mapping and persistence.',
+        'Deployed Spring Boot-based microservices in Docker and Amazon EC2 container using Jenkins. Implemented unit test cases using JUnit, and Mockito, and performed automation testing using Selenium to ensure the quality of Java-based applications deployed on AWS.',
+        'Utilized Apache Kafka to enable real-time data streaming and processing, resulting in a 40% improvement in system performance and reducing API response times by 30%. Successfully handled over 100,000 messages per second, ensuring scalable and reliable communication between distributed services.',
         'Employed React, JavaScript to develop dynamic and responsive user interfaces, using its component-based architecture to modularize UI elements and facilitate code reuse across the application.',
+        'Created and optimized SQL queries, indexes, and stored procedures for efficient data retrieval and processing.',
+        'Assisted in the documentation of code, architecture, and other technical specifications, ensuring that all team members can easily understand the code and contribute to the project.',
       ],
       expanded: false,
     },
+    
   ])
 
   const handleClick = (index) => {
