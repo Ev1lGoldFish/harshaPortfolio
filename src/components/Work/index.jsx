64312b6a61ca3cc05f1da6f2a2b4707437020f67
Ev1lGoldFish <63042619+Ev1lGoldFish@users.noreply.github.com>
2024-11-01 16:59:52 -0400

    Update index.jsx

--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -20,7 +20,7 @@ const Work = () => {
       role: 'Full Stack Developer',
       description: [
         "Spearheaded front-end development for the organization's web application utilizing React and Tailwind and achieved a significant increase in user interaction metrics with a boost in daily active users reaching over 2,000.",
-        'Implemented REST APIs in Java with Spring Boot to provide content to front-end, ensuring seamless data handling and improved server response times for thousands of monthly users.',
+        'Developed and optimized scalable backend solutions using Node.js and Express.js, enabling efficient data processing and significantly enhancing server response times to support thousands of active users each month.',
         "Developed a secure authentication system utilizing JWT that protected user data for over 10,000 monthly users and ensured compliance with GDPR and CCPA regulations while reinforcing trust in the platform's security measures.",
         'Coordinated the deployment and scaling of the application using Docker and AWS EC2, achieving consistent performance and reliability across multiple production environments.',
         'Participated in full SDLC including requirements gathering, database schema design, and code optimization for enhanced application performance and stability.'
